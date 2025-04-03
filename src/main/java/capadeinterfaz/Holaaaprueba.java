a51fe8f3af69575350b6f1923785b2f5143dd019
LAB2_PC18 <LAB2_PC18@LAB02-PC18.utlvte.int>
2025-04-03 11:23:40 -0500

    prueba

---
+++ src/main/java/capadeinterfaz/Holaaaprueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capadeinterfaz;
+
+/**
+ *
+ * @author LAB2_PC18
+ */
+public class Holaaaprueba {
+    /* Prueba gholaaaaaaaaa*/
+    
+    
+    
+    
+    
+    
+    
+}
